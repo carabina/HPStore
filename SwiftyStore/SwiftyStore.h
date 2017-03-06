0be3e7c66b3604dee3630eda31b3f3d839b2536e
Fri3ndlyGerman <pan1008406@gmail.com>
2017-03-06 15:33:30 +0100

    Initial Commit

---
+++ SwiftyStore/SwiftyStore.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyStore.h
+//  SwiftyStore
+//
+//  Created by Henrik Panhans on 06.03.17.
+//  Copyright © 2017 Henrik Panhans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyStore.
+FOUNDATION_EXPORT double SwiftyStoreVersionNumber;
+
+//! Project version string for SwiftyStore.
+FOUNDATION_EXPORT const unsigned char SwiftyStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyStore/PublicHeader.h>
+
+
